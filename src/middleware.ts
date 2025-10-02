f49faf71f7aa37c20231f3b5ac34880d066b507a
EmranAli-15 <mohammademranali15@gmail.com>
2025-10-02 13:30:02 +0600

    initial auth route done

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,30 @@
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
+    const outRoutes = ["/login", "/register"];
+    const authRoutes = ["/orderProcess"]
 
-    return NextResponse.next();
+    const authToken = (await cookies()).get("token")?.value;
+    let decoded = null;
+    if (authToken) {
+        decoded = jwtDecode(authToken);
+    };
+
+    if (decoded) {
+        const result = outRoutes.includes(pathname);
+        if (result)
+            return NextResponse.redirect(new URL("/", request.url))
+        else
+            return NextResponse.next()
+    }
+    else {
+        const result = authRoutes.includes(pathname);
+        if (result)
+            return NextResponse.redirect(new URL("/login", request.url))
+        else
+            return NextResponse.next()
+    }
 }
