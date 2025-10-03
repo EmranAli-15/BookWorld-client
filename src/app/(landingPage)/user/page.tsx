bfbc01c5b833b86f48c73a04021501fc375d5e3d
EmranAli-15 <mohammademranali15@gmail.com>
2025-10-03 12:37:16 +0600

    authorization working

--- src/app/(landingPage)/user/page.tsx
+++ src/app/(landingPage)/user/page.tsx
@@ -3,22 +3,32 @@
 import Container from '@/components/Container'
 import { useUser } from '@/contextProvider/ContextProvider'
 import { useGetUserQuery } from '@/redux/features/userApi'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function page() {
   const { user } = useUser();
+  const [error, setError] = useState("");
 
-  const { data, isError, isLoading, isSuccess } = useGetUserQuery(user?.userId);
+  const { data, isError, isLoading, isSuccess, error: resErr } = useGetUserQuery(user?.userId);
+
+  useEffect(() => {
+    if (isError) {
+      const err = resErr as { data: { message: string } }
+      setError(err.data.message)
+    }
+  }, [isError])
+
 
   let content = null;
   if (isLoading) {
     content = <h1>Loading...</h1>
   }
   else if (!isLoading && isError) {
-    content = <h1>Something Wrong...</h1>
+    content = <div role="alert" className="alert alert-error alert-soft">
+      <span>{error}</span>
+    </div>
   }
   else if (!isLoading && !isError && isSuccess) {
-    console.log(data.data);
     content = <h1>User Fetched.</h1>
   }
 
--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -1,11 +1,26 @@
 // Or from '@reduxjs/toolkit/query' if not using the auto-generated hooks
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import Cookies from 'js-cookie';
 
 const baseURL = "http://localhost:5000/api"
 
 // initialize an empty api service that we'll inject endpoints into later as needed
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: baseURL }),
+  baseQuery: fetchBaseQuery(
+    {
+      baseUrl: baseURL,
+      prepareHeaders: (headers, { getState }) => {
+        const token = Cookies.get("token");
+
+        // If we have a token set in state, let's assume that we should be passing it.
+        if (token) {
+          headers.set('token', token);
+        }
+
+        return headers
+      },
+    },
+  ),
   endpoints: () => ({}),
   tagTypes: ["getUser"]
 })
