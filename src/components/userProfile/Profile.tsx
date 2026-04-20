ad17423357171c7f021f2627f0587b4088827fb7
EmranAli-15 <mohammademranali15@gmail.com>
2026-04-20 22:39:12 +0600

    updating user profile

---
+++ src/components/userProfile/Profile.tsx
@@ -0,0 +1,175 @@
+"use client"
+import Cookies from 'js-cookie'
+
+import Container from '@/components/Container'
+import { useUser } from '@/contextProvider/ContextProvider'
+import { CameraIcon, UserIcon } from '@/icons/Icons'
+import { useGetUserQuery, useUpdataUserMutation } from '@/redux/features/userApi'
+import { uploadImage } from '@/utils/uploadImage'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { signOut } from 'next-auth/react'
+
+export default function userProfile() {
+  const router = useRouter();
+  const { user, setLoading } = useUser();
+
+  const [error, setError] = useState("");
+  const [updateLoading, setUpdateLoding] = useState(false);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [image, setImage] = useState("");
+  const [address, setAddress] = useState("");
+  const [updateImage, SetUpdateImage] = useState<File | "">("");
+
+
+
+  const { data, isError, isLoading, isSuccess, error: resErr } = useGetUserQuery(user?.userId);
+  const [updateUserFn, { isError: updateError, isSuccess: updateSuccess }] = useUpdataUserMutation();
+
+
+  const handlePhoto = (e: File) => {
+    setImage(URL.createObjectURL(e));
+    SetUpdateImage(e);
+  };
+
+  const handleUpdate = async () => {
+    setUpdateLoding(true);
+    let newImage = "";
+    if (updateImage) {
+      newImage = await uploadImage(updateImage) || "";
+    };
+
+    const data = {
+      name, phone, address
+    } as any;
+
+    if (newImage) data.image = newImage
+
+    const finalData = { id: user?.userId, data }
+    updateUserFn(finalData);
+  };
+  useEffect(() => {
+    setUpdateLoding(false);
+  }, [updateError, updateSuccess])
+
+
+
+
+  useEffect(() => {
+    if (isError) {
+      const err = resErr as { data: { message: string } }
+      setError(err.data.message)
+    };
+
+    if (isSuccess) {
+      setName(data.data.name)
+      setEmail(data.data.email)
+      setPhone(data.data.phone || "")
+      setAddress(data.data.address || "")
+      setImage(data.data.image)
+    }
+  }, [isError, isSuccess]);
+
+  useEffect(() => {
+    document.title = "My Profile"
+  }, []);
+
+
+  const handleLogout = () => {
+    signOut();
+    router.push("/");
+    Cookies.remove("token");
+    setLoading(true);
+  }
+
+
+  let content = null;
+  if (isLoading) {
+    content = <h1>Loading...</h1>
+  }
+  else if (!isLoading && isError) {
+    content = <div role="alert" className="alert alert-error alert-soft">
+      <span>{error}</span>
+    </div>
+  }
+  else if (!isLoading && !isError && isSuccess) {
+    content = <div>
+      <div className='lg:flex gap-x-2'>
+        <div className='lg:w-1/2 flex lg:flex-col justify-between'>
+          <div className='relative'>
+            <div className='rounded-full size-[200px]'>
+              {
+                image ? <Image className='size-[200px] object-cover rounded-full' height={200} width={200} src={image} alt={name}></Image> :
+                  <UserIcon w={200}></UserIcon>
+              }
+            </div>
+
+            <label className='absolute bottom-0 left-[180px] cursor-pointer hover:text-blue-700' htmlFor="inputImage">
+              <CameraIcon></CameraIcon>
+            </label>
+            <input onChange={(e) => handlePhoto(e.target.files![0])} className='hidden' type="file" id="inputImage" />
+          </div>
+          <div>
+            <button
+              onClick={() => handleLogout()}
+              className="btn btn-soft btn-warning">
+              Log Out
+            </button>
+          </div>
+        </div>
+        <div className='lg:w-1/2 mt-5 lg:mt-0'>
+          <label htmlFor="">Name:</label>
+          <input
+            className='border outline-0 p-1 w-full rounded'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+          />
+
+          <p className='mt-2'>Email:</p>
+          <input
+            className='border border-gray-200 text-gray-400 cursor-not-allowed outline-0 p-1 w-full rounded'
+            value={email}
+            disabled
+          />
+
+          <p className='mt-2'>Phone:</p>
+          <input
+            className='border outline-0 p-1 w-full rounded'
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            type="text"
+            placeholder='Enter your phone number'
+          />
+
+          <p className='mt-2'>Address:</p>
+          <textarea
+            className='border outline-0 p-1 w-full rounded'
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            placeholder='Write your shipping address'
+          />
+
+          <button onClick={handleUpdate} disabled={updateLoading} className="btn btn-soft btn-accent w-full">
+            {
+              updateLoading ? "updating ..." : "update"
+            }
+          </button>
+
+        </div>
+      </div>
+    </div>
+  }
+
+  return (
+    <Container>
+      <div className='my-5'>
+        {content}
+      </div>
+    </Container>
+  )
+}
