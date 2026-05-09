31f9859aa4e274d77bff05a8366f43c3491e980a
EmranAli-15 <mohammademranali15@gmail.com>
2026-05-10 00:29:59 +0600

    facing login error in production

--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import Cookies from 'js-cookie';
 
-const baseURL = "http://localhost:5000/api"
-// const baseURL = "https://bookworld-server.vercel.app/api"
+// const baseURL = "http://localhost:5000/api"
+const baseURL = "https://bookworld-server.vercel.app/api"
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery(
